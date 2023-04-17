35199a005d4fa4af98cb3970b98c03e2a0bc1478
djadi <djadi@DVIJENIELS>
2023-04-17 22:56:33 +0300

    Create XML for Google

---
+++ OnlineMagazin/Controllers/GoogleMerchantCenter.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineMagazin.Data;
+
+namespace MyApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(Roles = "Admin")]
+    public class GoogleMerchantCenterController : ControllerBase
+    {
+        private readonly OnlineMagazinContext _context;
+        private readonly ILogger<GoogleMerchantCenterController> _logger;
+
+        public GoogleMerchantCenterController(ILogger<GoogleMerchantCenterController> logger, OnlineMagazinContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult GetProductsXml()
+        {
+            var products = new List<GoogleProduct>();
+            foreach (var product in _context.Products.ToList())
+            {
+                var doc = new HtmlDocument();
+                doc.LoadHtml(product.Icerik.Substring(0, 240));
+                string plainText = doc.DocumentNode.InnerText;
+                plainText = Regex.Replace(plainText, @"\s+", " ");
+                plainText = Regex.Replace(plainText, @"&laquo;|&raquo;|&nbsp;|&", " ");
+                var offer = new GoogleProduct
+                {
+                    Id = product.ProductId.ToString(),
+                    Title = product.Baslik,
+                    Description = plainText,
+                    Link = "https://pskanker.ru/Home/ProductDetail/" + product.ProductId,
+                    ImageLink = "https://pskanker.ru/image/" + product.Foto,
+                    Condition = "new",
+                    Availability = "in_stock",
+                    Price = product.Price+" RUB",
+                    Brand = "Pskanker",
+                    UpdateType= "merge",
+                    Gtin="151512",
+                    Mpn="161613"
+                };
+
+                products.Add(offer);
+            }
+            var xml = new XElement("rss",
+            new XAttribute("version", "2.0"),
+            new XElement("channel",
+                new XElement("title", "Все товары"),
+                new XElement("link", "https://pskanker.ru/Home/GetProducts"),
+                new XElement("description", "Продукты - Vector Строй маркет"),
+                products.Select(product =>
+                    new XElement("item",
+                        new XElement("g_id", product.Id),
+                        new XElement("title", product.Title),
+                        new XElement("description", product.Description),
+                        new XElement("g_link", product.Link),
+                        new XElement("g_image_link", product.ImageLink),
+                        new XElement("g_condition", product.Condition),
+                        new XElement("g_availability", product.Availability),
+                        new XElement("g_price", product.Price),
+                        new XElement("g_brand", product.Brand),
+                        new XElement("g_gtin", product.Gtin),
+                        new XElement("g_mpn", product.Mpn),
+                        new XElement("g_update_type", product.UpdateType)
+                        )
+                    )
+                )
+            );
+            return Content(xml.ToString(), "application/xml");
+        }
+
+    }
+    public class GoogleProduct
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public string ImageLink { get; set; }
+        public string Condition { get; set; }
+        public string Availability { get; set; }
+        public string Price { get; set; }
+        public string Brand { get; set; }
+        public string UpdateType { get; set; }
+        public string Gtin { get; set; }
+        public string Mpn { get; set; }
+    }
+}
---
+++ OnlineMagazin/Controllers/YandexMarket.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Serialization;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineMagazin.Data;
+
+namespace MyApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(Roles = "Admin")]
+    public class YandexMarketController : ControllerBase
+    {
+        private readonly OnlineMagazinContext _context;
+        private readonly ILogger<YandexMarketController> _logger;
+
+        public YandexMarketController(ILogger<YandexMarketController> logger, OnlineMagazinContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            var feed = new YandexMarket
+            {
+                Version = "2.0",
+                Shop = new YandexMarketShop
+                {
+                    Name = "Vector Строй Маркет",
+                    Company = "Строительный Маркет",
+                    Url = "https://pskanker.ru",
+                    Currencies = new List<YandexMarketCurrency>
+                    {
+                        new YandexMarketCurrency()
+                        {
+                            Id = "RUB",
+                            Rate = "1"
+                        }
+                    },
+                    Categories = new List<YandexMarketCategory>(),
+                    Offers = new List<YandexMarketOffer>()
+                }
+            };
+
+            foreach (var category in _context.Category.ToList())
+            {
+                string categoryText = Regex.Replace(category.CategoriName, "&", "");
+                var offerCategory = new YandexMarketCategory
+                {
+                    Id = category.CategoryId.ToString(),
+                    Name = categoryText,
+                };
+
+                feed.Shop.Categories.Add(offerCategory);
+            }
+            foreach (var product in _context.Products.ToList())
+            {
+                var doc = new HtmlDocument();
+                doc.LoadHtml(product.Icerik.Substring(0, 240));
+                string plainText = doc.DocumentNode.InnerText;
+                plainText = Regex.Replace(plainText, @"\s+", " ");
+                plainText = Regex.Replace(plainText, @"&laquo;|&raquo;|&nbsp;|&", " ");
+                var offer = new YandexMarketOffer
+                {
+                    Id = product.ProductId.ToString(),
+                    Available = "true",
+                    Url = "https://pskanker.ru/Home/ProductDetail/" + product.ProductId,
+                    Price = (decimal)product.Price,
+                    CurrencyId = "RUB",
+                    CategoryId = product.CategoryId.ToString(),
+                    Picture = "https://pskanker.ru/image/" + product.Foto,
+                    Pickup= "true",
+                    Delivery = "true",
+                    Name = product.Baslik,
+                    Description = plainText,
+                };
+
+                feed.Shop.Offers.Add(offer);
+            }
+            var settings = new XmlWriterSettings
+            {
+                Encoding = Encoding.UTF8,
+                Indent = true
+            };
+
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = XmlWriter.Create(stream, settings))
+                {
+                    var serializer = new XmlSerializer(typeof(YandexMarket));
+                    serializer.Serialize(writer, feed);
+                }
+                return File(stream.ToArray(), "application/xml");
+            }
+        }
+    }
+
+    [XmlRoot(ElementName = "yml_catalog")]
+    public class YandexMarket
+    {
+        [XmlAttribute(AttributeName = "date")]
+        public string Date { get; set; } = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm");
+
+        [XmlAttribute(AttributeName = "version")]
+        public string Version { get; set; }
+
+        [XmlElement(ElementName = "shop")]
+        public YandexMarketShop Shop { get; set; }
+    }
+
+    public class YandexMarketShop
+    {
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+
+        [XmlElement(ElementName = "company")]
+        public string Company { get; set; }
+
+        [XmlElement(ElementName = "url")]
+        public string Url { get; set; }
+
+        [XmlArray("currencies")]
+        [XmlArrayItem("currency")]
+        public List<YandexMarketCurrency> Currencies { get; set; }
+
+        [XmlArray("categories")]
+        [XmlArrayItem("category")]
+        public List<YandexMarketCategory> Categories { get; set; }
+
+        [XmlArray("offers")]
+        [XmlArrayItem("offer")]
+        public List<YandexMarketOffer> Offers { get; set; }
+    }
+
+    public class YandexMarketCurrency
+    {
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+
+        [XmlAttribute(AttributeName = "rate")]
+        public string Rate { get; set; }
+    }
+
+    public class YandexMarketCategory
+    {
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+
+        [XmlAttribute(AttributeName = "parentId")]
+        public string ParentId { get; set; }
+
+        [XmlText]
+        public string Name { get; set; }
+    }
+
+    public class YandexMarketOffer
+    {
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+
+        [XmlAttribute(AttributeName = "available")]
+        public string Available { get; set; }
+
+        [XmlElement(ElementName = "url")]
+        public string Url { get; set; }
+
+        [XmlElement(ElementName = "price")]
+        public decimal Price { get; set; }
+
+        [XmlElement(ElementName = "currencyId")]
+        public string CurrencyId { get; set; }
+
+        [XmlElement(ElementName = "categoryId")]
+        public string CategoryId { get; set; }
+
+        [XmlElement(ElementName = "picture")]
+        public string Picture { get; set; }
+
+        [XmlElement(ElementName = "pickup")]
+        public string Pickup { get; set; }
+
+        [XmlElement(ElementName = "delivery")]
+        public string Delivery { get; set; }
+
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+    }
+
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string CategoryId { get; set; }
+    }
+}
